5c428a7fcfefccb4f8888f6bc691a37750937c2e
Udo chimgozirim <pchikezie2000@gmail.com>
2026-09-02 01:41:53 +0100

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+
+export default function App() {
+
+
+  return <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>;
+}
